8bdaa578fdc73ccebf51ac2bc0e60c692d96a791
VictorStankov <viktorstankov1@gmail.com>
2022-03-06 20:13:29 +0200

    Fix mic toggling script

--- .config/polybar/mic-toggle.sh
+++ .config/polybar/mic-toggle.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pactl set-source-mute alsa_input.usb-Logitech_PRO_X_000000000000-00.mono-fallback toggle
-STATUS=$(pactl list sources | grep -A 5 'Description: PRO X Mono' | grep 'Mute' | grep -E -o "yes|no")
+STATUS=$(pactl list sources | grep -A 5 'Description: Logitech G PRO X Gaming Headset Mono' | grep 'Mute' | grep -E -o "yes|no")
 if [[ $STATUS = "no" ]]; then
 	polybar-msg hook mic-toggle 1
 elif [[ $STATUS = "yes" ]]; then
